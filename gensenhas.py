fa21a140bbb190af2a7a11e6c2247c6fb334d352
JMartiniano <jose.martiniano01@gmail.com>
2022-03-18 12:39:40 -0300

    Updates

--- gensenhas.py
+++ gensenhas.py
@@ -1,16 +1,50 @@
-# Importando biblioteca para gerar números aleatórios
+# Import library of random
 from random import randint
 
-# Abrindo o arquivo onde serão gravadas as senhas geradas
-arqsenhas = open('./senhas.txt', 'a')
+# How many digits password
+tam = int(input('How many digist in the password? '))
 
-# Definindo a quantidade de dígitos por senha
-tam = 8
+# Array to save the passwords 
+passwords = []
 
-# For que irá gerá 643 senhas aleatórias de 8 dígitos cada
-for i in range(643):
-    senha = int(''.join(str(randint(0,9)) for _ in range(tam)))
-    while len(str(senha)) != tam:
-        senha = int(''.join(str(randint(0,9)) for _ in range(tam)))
-    # Escreve a senha gerada no arquivo que foi criado antes
-    arqsenhas.write(f"{str(senha)}\n")
+# How many passwords to generating
+howmany = int(input('How many passwords to genarating? '))
+
+
+writeYN = input('Do you want write the passwords on a .txt? [y,n] ')
+
+if writeYN == "y" or writeYN == "Y":
+    # Asking for the path
+    path = input('Path to the file: ')
+    # Opening the file
+    arqpasswords = open(path, 'w')
+
+#Asking about fomart
+print (f'\nFormat option A:\n--------------\n[01] - password\n--------------\n[02] - password\n--------------\n')
+print (f'\nFormat option B:\npassword\npassword\n')
+optformat = input('Choice a format option: [A,B]')
+
+print ('-' * (tam+(len(str(howmany)))+7))
+
+# For that will create howmany random passwords with tam digits each
+for i in range(howmany):
+    
+    # Generating passwords
+    password = int(''.join(str(randint(0,9)) for _ in range(tam)))
+    
+    # Saving passwords in the array
+    passwords.append(password)
+
+    if writeYN == 'y' or writeYN == 'Y':
+
+        if optformat == "b" or optformat == "B":
+            
+            # Writing the passwords
+            arqpasswords.write(f"{str(passwords[i])}\n")        
+
+        elif optformat == "a" or optformat == "A":
+            arqpasswords.write(f"[{i+1:0>4}] - {passwords[i]:0>6}\n" + '-' * (tam+(len(str(howmany)))+7) + '\n')
+
+    else:
+        print (f"[{i+1:0>4}] - {passwords[i]:0>6}")
+        print ('-' * (tam+(len(str(howmany)))+7))
